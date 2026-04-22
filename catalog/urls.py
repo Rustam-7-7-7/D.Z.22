93f6db048131b8c33a5493a2dd47a9ea3d8f018b
safin <safin@example.com>
2026-04-23 01:13:11 +0300

    homework_24 done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from catalog import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('index/', views.index, name='index'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -8,3 +8,16 @@ def home(request):
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+
+from django.shortcuts import get_object_or_404
+from .models import Product
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
+
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'catalog/index.html', {'products': products})
