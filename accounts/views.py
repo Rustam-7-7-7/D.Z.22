9dbe705151dc76e3e98ad661e3f3061b9338113b
safin <safin@example.com>
2026-05-09 20:11:25 +0300

    homework_27 done

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.core.mail import send_mail
 from .forms import CustomUserCreationForm
+from django.conf import settings
 
 def register(request):
     if request.method == 'POST':
@@ -8,7 +10,14 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            # Логика отправки приветственного письма
+            # Отправка приветственного письма
+            send_mail(
+                'Добро пожаловать на наш сайт!',
+                'Спасибо за регистрацию на нашем сайте.',
+                settings.EMAIL_HOST_USER,
+                [user.email],
+                fail_silently=False,
+            )
             return redirect('product_list')  # Убедитесь, что 'product_list' - это имя существующего маршрута
     else:
         form = CustomUserCreationForm()
--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@yandex.ru'  # Замените на вашу Яндекс почту
+EMAIL_HOST_PASSWORD = 'your_password'     # Замените на ваш пароль
